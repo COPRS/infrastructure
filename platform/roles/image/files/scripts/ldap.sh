4ef7bcf679e940a43225808ee7d5c2b369500be8
William RUFFINE <william.ruffine@csgroup.eu>
2021-12-01 11:29:48 +0100

    feat[auth]: add LDAP packages in base image

---
+++ platform/roles/image/files/scripts/ldap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install necessary ldap packages
+sudo DEBIAN_FRONTEND=nointerractive \
+apt install -y ldap-auth-client=0.5.4 \
+ldap-auth-config=0.5.4 \
+libnss-ldap=265-5ubuntu1 \
+libpam-ldap=186-4ubuntu1 \
+ldap-utils=2.4.49+dfsg-2ubuntu1.8
