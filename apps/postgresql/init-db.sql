60f9bb4d1d147fbf898405d6ae153a6968f665e0
Aubin Lambaré <aubin.lambare@csgroup.eu>
2022-01-25 11:27:27 +0100

    refactor; update to new app installer (mongodb, openldap, postgresql)

--- apps/postgresql/init-db.sql
+++ apps/postgresql/init-db.sql
@@ -1,6 +1,6 @@
-CREATE USER keycloak WITH ENCRYPTED PASSWORD 'keycloakpassword';
-CREATE DATABASE keycloak;
-GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
+CREATE USER {{ keycloak.database.user }} WITH ENCRYPTED PASSWORD '{{ keycloak.database.password }}';
+CREATE DATABASE {{ keycloak.database.name }};
+GRANT ALL PRIVILEGES ON DATABASE {{ keycloak.database.name }} TO {{ keycloak.database.user }};
 CREATE USER scdf WITH ENCRYPTED PASSWORD 'scdfpassword';
 CREATE DATABASE skipper;
 GRANT ALL PRIVILEGES ON DATABASE skipper TO scdf;
